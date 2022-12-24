672f9724433fcc9616f9e3811fc22dae9dd7ab4c
pedrobarretto <pedro@barretto.com.br>
2022-12-24 00:25:20 -0300

    feat: add pressable func, add winning check on all horizontal lines, vertical not working yet

--- App.tsx
+++ App.tsx
@@ -1,16 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, View, ImageBackground, Image } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, ImageBackground, Image, Pressable } from 'react-native';
 import board from './assets/bg.png';
 import xImg from './assets/x.png';
 import oImg from './assets/o.png';
 
 export default function App() {
+  const [turn, setTurn] = useState<'x' | 'o'>('x');
   const [table, setTable] = useState([
-    ['o', '', 'o'],
-    ['x', '', 'x'],
-    ['', '', 'o']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
   ]);
+
+  const emptyTable = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+
+  const makeMove = (row: number, cell: number) => {
+    const pos = table[row][cell];
+
+    if (pos !== '') return;
+
+    let newTable = [];
+    table[row][cell] = turn;
+    newTable = table;
+
+    setTable(newTable);
+    setTurn(turn === 'x' ? 'o' : 'x');
+  }
+
+  function allEqual(arr: string[]) {
+    return new Set(arr).size == 1;
+  }
+
+  const checkWinner = () => {
+    // Check if winner is on a row
+    console.debug('Checking winner...');
+    table.map((row, rIndex) => {
+      // Check strait row
+      if (row[rIndex] !== '' && allEqual(row)) {
+        console.debug('Win on horizontal!');
+        setTable(emptyTable);
+        return;
+      }
+    });
+
+    // FIXME: Not working :(
+    if (table[0][0] === '' && table[0][1] === '' && table[0][2] === '') return;
+
+    if (
+      table[0][0] === table[0][1] &&
+      table[0][0] === table[0][2] &&
+      table[0][1] === table[0][2]
+      ) {
+        console.debug('Win on first vertical!');
+        setTable(emptyTable);
+        return;
+      }
+  }
+
+  useEffect(() => {
+    checkWinner();
+  }, [table, turn]);
 
   return (
     <View style={styles.container}>
@@ -18,16 +72,16 @@ export default function App() {
 
       <View style={styles.grid}>
         {
-          table.map((row) => {
+          table.map((row, rIndex) => {
             return (
-              <View style={styles.row}>
+              <View key={rIndex} style={styles.row}>
                 {
-                  row.map((cell) => {
+                  row.map((cell, cIndex) => {
                     return (
-                      <View style={styles.cell}>
+                      <Pressable key={cIndex} style={styles.cell} onPress={() => makeMove(rIndex, cIndex)}>
                         {cell === 'o' && <Image source={oImg} style={[styles.img, styles.oImg]} />}
                         {cell === 'x' && <Image source={xImg} style={[styles.img, styles.xImg]} />}
-                      </View>
+                      </Pressable>
                     )
                   })
                 }
@@ -59,15 +113,13 @@ const styles = StyleSheet.create({
   },
   xImg: {
     marginTop: 15,
-    marginLeft: 15
+    marginLeft: 25
   },
   oImg: {
-    marginTop: 15,
-    marginLeft: 15
+    marginTop: 13,
+    marginLeft: 12
   },
   grid: {
-    borderWidth: 1,
-    borderColor: 'red',
     width: '80%',
     aspectRatio: 1,
     position: 'absolute'
